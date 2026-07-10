fac6457c435657652c28332c3200c8fa3d0a2b6f
jecharc-beep <jecharc@gmail.com>
2026-07-10 15:42:38 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 //  Permite uso offline completo
 // ═══════════════════════════════════════════════
 
-const CACHE_NAME = 'dictamen-forestal-v21';
+const CACHE_NAME = 'dictamen-forestal-v22';
 const BASE = '/dictamen-forestal';
 
 // Archivos a cachear para uso offline
